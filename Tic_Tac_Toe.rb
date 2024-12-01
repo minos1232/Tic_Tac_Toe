245799e96eaad5cc9f9f604236a2af65c2b5f4e7
amine <mesbahminos@gmail.com>
2024-12-01 23:31:39 +0100

    creat the game class and display board

--- Tic_Tac_Toe.rb
+++ Tic_Tac_Toe.rb
@@ -1,1 +1,19 @@
 # Tic Tac Toe
+class TicTacToe
+  attr_accessor :game_board
+  def initialize
+    @game_board = Array.new(9, " ")
+  end
+
+  def display_board
+    puts "\n"
+    puts " #{game_board[0]} | #{game_board[1]} | #{game_board[2]} "
+    puts "---|---|---"
+    puts " #{game_board[3]} | #{game_board[4]} | #{game_board[5]} "
+    puts "---|---|---"
+    puts " #{game_board[6]} | #{game_board[7]} | #{game_board[8]} "
+    puts "\n"
+  end
+end
+game = TicTacToe.new
+game.display_board
