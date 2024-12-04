56a953572b8f82cfb50f53357f5e0b64c51af094
amine <mesbahminos@gmail.com>
2024-12-04 20:37:29 +0100

    adding player movement and draw case

--- Tic_Tac_Toe.rb
+++ Tic_Tac_Toe.rb
@@ -14,6 +14,46 @@ class TicTacToe
     puts " #{game_board[6]} | #{game_board[7]} | #{game_board[8]} "
     puts "\n"
   end
+
+  def player_move(player)
+    loop do
+      print "#{player}, choose a position (1-9): "
+      position = gets.chomp.to_i - 1
+      if valid_move?(position)
+        game_board[position] = player
+        break
+      else
+        puts "Invalid move. Try again."
+      end
+    end
+  end
+
+  def valid_move?(position)
+    position.between?(0, 8) && game_board[position] == " "
+  end
+
+  def full_board?
+    !game_board.include?(" ") 
+  end
+
+  def play
+    puts "Welcome to Tic Tac Toe!"
+    display_board
+    current_player = "X"
+
+    loop do
+      player_move(current_player)
+      display_board
+
+      if full_board?
+        puts "It's a draw!"
+        break
+      else
+        current_player = current_player == "X" ? "O" : "X"
+      end
+    end
+  end
 end
+
 game = TicTacToe.new
-game.display_board
+game.play
